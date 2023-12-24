9ed00b7281582112eacff29bfa79352957f64421
vishesh-baghel <visheshbaghel99@gmail.com>
2023-12-24 15:28:46 +0530

    added new fetch function with installation id param

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,14 +1,48 @@
 import { Octokit } from "octokit";
 import { RequestParameters } from "@octokit/types";
 import { Probot } from "probot";
+import { createAppAuth } from "@octokit/auth-app";
+import { error } from "console";
 
 const token = process.env.GITHUB_ACCESS_TOKEN;
+const appId = process.env.APP_ID;
+const privateKey = process.env.PRIVATE_KEY;
 
 const octokitApp = new Octokit({
   auth: token,
 });
 
-export default function fetchDetails(
+function fetchDetailsWithInstallationId(
+  app: Probot,
+  installationId: number,
+  endpoint: string,
+  parameters: RequestParameters
+) {
+  const octokit = new Octokit({
+    authStrategy: createAppAuth,
+    auth: {
+      appId: appId,
+      privateKey: privateKey,
+      installationId: installationId,
+    },
+  });
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      app.log.info("fetching details for resource endpoint: " + endpoint);
+      const data = await octokit.request(endpoint, parameters);
+
+      resolve(data);
+    } catch (err: any) {
+      app.log.error(
+        `Error while fetching resource details for resource url: ${endpoint}`
+      );
+      reject(error);
+    }
+  });
+}
+
+function fetchDetails(
   app: Probot,
   endpoint: string,
   parameters: RequestParameters
@@ -27,6 +61,8 @@ export default function fetchDetails(
     }
   });
 }
+
+export { fetchDetails, fetchDetailsWithInstallationId };
 
 // const octokit = new Octokit({
 //   auth: "YOUR-TOKEN",
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,69 @@
 import { Probot } from "probot";
 import connectDb from "./dbConnection";
 import listeningForAppInstallationEvents from "./listeners/appInstallationListener";
-import fetchDetails from "./fetch";
 import configs from "./configs/fetch.configs.json";
+import { fetchDetailsWithInstallationId, fetchDetails } from "./fetch";
+
+type repository = {
+  id: number;
+  node_id: string;
+  name: string;
+  full_name: string;
+  private: boolean;
+};
 
 function main(app: Probot) {
   connectDb(app).catch((error: any) => {
     app.log.error(error);
   });
 
-  listeningForAppInstallationEvents(app).then((data: any) => {
-    app.log.info(data);
-  });
+  listeningForAppInstallationEvents(app)
+    .then((data: any) => {
+      const { repositories, installation } = data;
+      const repos: [repository] = repositories;
+      const installationId: number = installation.id;
 
-  fetchDetails(app, configs.endpoints.all_issues, {
-    owner: "ravgeetdhillon",
-    repo: "dropilio",
+      // Confirmed from https://github.com/orgs/community/discussions/24509
+      const owner = installation.account.login;
+
+      repos.forEach((repo) => {
+        fetchDetailsWithInstallationId(
+          app,
+          installationId,
+          configs.all_issues.endpoint,
+          {
+            owner: owner,
+            repo: repo.name,
+            // state: configs.all_issues.state_closed,
+            // labels: configs.all_issues.label_bug,
+            headers: {
+              "X-GitHub-Api-Version": "2022-11-28",
+            },
+          }
+        )
+          .then((response: any) => {
+            app.log.info(response);
+            const data = response.data;
+            data.forEach((obj: any) => {
+              app.log.info(`Issue title ${obj.title}`);
+            });
+          })
+          .catch((error: any) => {
+            app.log.error(error);
+          });
+      });
+    })
+    .catch((error: any) => {
+      app.log.error("Error while processing app installation event");
+      throw error;
+    });
+
+  fetchDetails(app, configs.endpoints.get_issue, {
+    owner: "vishesh-baghel",
+    repo: "glide",
+    issue_number: 1,
   }).then((data: any) => {
-    app.log.info(data);
+    // app.log.info(data);
   });
 }
 
