1cadd4221223f49e1b5b3aebe2da873c79a1f705
vishesh-baghel <visheshbaghel99@gmail.com>
2024-01-10 09:10:34 +0530

    added a demo comment to show raw scores

--- src/constants/Comments.ts
+++ src/constants/Comments.ts
@@ -4,12 +4,22 @@ export enum CommentType {
   Error = "error",
 }
 
-export function pullRequestOpenComment(rows: string[][]): DataObject[] {
+/**
+ * Function to generate a comment for an open pull request.
+ *
+ * @param rows - An array of file information.
+ * @param rawScores - An array of raw risk scores.
+ * @returns An array of data objects containing comments and tables.
+ */
+export function pullRequestOpenComment(
+  rows: string[][],
+  rawScores: string[][]
+): DataObject[] {
   return [
-    { h2: "Pay more attention while reviewing these files" },
+    { h2: "Attention: Review Required for the Following Files" },
     {
       blockquote:
-        "This curated list helps you focus on files that may have significant issues. Files are prioritized by their risk scores. A zero 'current risk score' or a zero 'predicted risk score' may indicate a new file or insufficient historical data. Your attention to these files is greatly appreciated!",
+        "This curated list is designed to help you focus on files that may require you to pay more attention while reviewing the code. Files are prioritized based on their risk scores. A zero 'current risk score' or a zero 'predicted risk score' may indicate a new file or insufficient historical data. Your attention to these files is highly appreciated!",
     },
     {
       table: {
@@ -17,13 +27,33 @@ export function pullRequestOpenComment(rows: string[][]): DataObject[] {
         rows: rows,
       },
     },
+    { h3: "For Demo Purposes Only" },
+    {
+      p: "The above comment reflects the real scenario, where scores are truncated to only two decimal places. For the demo, raw scores are included to provide insight into how the scores are calculated.",
+    },
+    {
+      table: {
+        headers: ["File Path", "Current Risk Score", "Predicted Risk Score"],
+        rows: rawScores,
+      },
+    },
   ];
 }
 
+/**
+ * Function to generate a fallback comment for a pull request open event in case of an error.
+ *
+ * @returns A string with a reassuring message about the encountered issue.
+ */
 export function errorFallbackCommentForPROpenEvent(): string {
-  return "We are facing some trouble at our end. Don't worry, we will show you the risk scores next time you open a pull request for sure.";
+  return "We are currently experiencing technical difficulties. Rest assured, we will display the risk scores the next time you open a pull request.";
 }
 
+/**
+ * Function to generate a fallback comment for a pull request closed event in case of an error.
+ *
+ * @returns A string with information about the error and assurance of upcoming risk score updates.
+ */
 export function errorFallbackCommentForPRClosedEvent(): string {
-  return "An error occurred while updating the files modified in this closed pull request. Don't worry, we have the backup for all the files in this pull request. The risk scores will be updated soon";
+  return "An error occurred while updating the files modified in this closed pull request. No need to worry; we have a backup for all the files in this pull request. The risk scores will be updated soon.";
 }
